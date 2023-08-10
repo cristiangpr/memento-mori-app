85961fbe1a456185ce26e3c6314867b796150a44
cristiangpr <cristiangp777@gmail.com>
2023-08-10 16:09:55 -0600

    set headers

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,30 +3,30 @@
 const path = require('path')
 const express = require('express')
 
-module.exports = function (app) {
-  const port = process.env.PORT || 3000
-  app.use(express.json())
-  app.use('/manifest.json', function (req, res, next) {
-    res.set({
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-    })
-
-    next()
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use('/manifest.json', function (req, res, next) {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
   })
 
-  // Your static pre-build assets folder
-  app.use(express.static(path.join(__dirname, '..', 'build')))
-  // Root Redirects to the pre-build assets
+  next()
+})
 
-  app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'build'))
-  })
-  // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'build/index.html'))
-  })
-  app.listen(port, () => {
-    console.log('Server is running on port: ', port)
-  }}
+// Your static pre-build assets folder
+app.use(express.static(path.join(__dirname, '..', 'build')))
+// Root Redirects to the pre-build assets
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build'))
+})
+// Any Page Redirects to the pre-build assets folder index.html that // will load the react app
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+})
+app.listen(port, () => {
+  console.log('Server is running on port: ', port)
+})
