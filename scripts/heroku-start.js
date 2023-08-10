4b9f8b409d2a01fa4da1395ad27bde9cdb7d6331
cristiangpr <cristiangp777@gmail.com>
2023-08-10 14:43:16 -0600

    set headers

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -4,9 +4,17 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*'])
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.append('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization')
+  next()
+})
+
 // Your static pre-build assets folder
 app.use(express.static(path.join(__dirname, '..', 'build')))
 // Root Redirects to the pre-build assets
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'build'))
 })
