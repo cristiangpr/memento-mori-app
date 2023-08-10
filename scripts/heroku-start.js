877914026119c2d70bc42700b5b8d2316b31d571
cristiangpr <cristiangp777@gmail.com>
2023-08-10 16:04:15 -0600

    set headers

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
-app.use('../manifest.json', function (req, res, next) {
+app.use('/manifest.json', function (req, res, next) {
   res.set({
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET',
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,11 @@
 // App is an express application, we can add an express middleware that will set headers for manifest.json request
 // https://create-react-app.dev/docs/proxying-api-requests-in-development/#configuring-the-proxy-manually
+const path = require('path')
+const express = require('express')
 
 module.exports = function (app) {
+  const port = process.env.PORT || 3000
+  app.use(express.json())
   app.use('/manifest.json', function (req, res, next) {
     res.set({
       'Access-Control-Allow-Origin': '*',
@@ -11,4 +15,18 @@ module.exports = function (app) {
 
     next()
   })
-}
+
+  // Your static pre-build assets folder
+  app.use(express.static(path.join(__dirname, '..', 'build')))
+  // Root Redirects to the pre-build assets
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'build'))
+  })
+  // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+  })
+  app.listen(port, () => {
+    console.log('Server is running on port: ', port)
+  }}
