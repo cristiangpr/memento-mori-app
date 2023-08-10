dc0cf458e7507475baa4da38103d7c4343edc004
cristiangpr <cristiangp777@gmail.com>
2023-08-10 17:19:28 -0600

    set headers

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
+
+app.use(cors())
 
 // Your static pre-build assets folder
 app.use(express.static(path.join(__dirname, '..', 'build')))
