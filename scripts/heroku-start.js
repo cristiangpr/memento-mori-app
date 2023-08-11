8ef7504b98f234f375e34bb0ef3d74ac284d73c8
cristiangpr <cristiangp777@gmail.com>
2023-08-11 11:29:43 -0600

    set headers

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,32 +1,18 @@
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-
-const app = express()
-const port = process.env.PORT || 3000
-app.use(express.json())
-
-app.use(function (req, res, next) {
-  res.st({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-  })
-
-  next()
-})
-
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
 // Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(express.static(path.join(__dirname, '..', 'build')));
 // Root Redirects to the pre-build assets
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'build'))
-})
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname, '..', 'build'));
+});
 // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'build/index.html'))
-})
-app.listen(port, () => {
-  console.log('Server is running on port: ', port)
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+});
+app.listen(port, ()=>{
+  console.log("Server is running on port: ", port)
 })
